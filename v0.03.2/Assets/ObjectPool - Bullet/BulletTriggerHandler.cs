663b0416577e9ca760abe99d25223a91751f50ba
devjyansei <cemregumusofficial@gmail.com>
2022-11-09 10:25:09 +0300

    Homing Missile Bugfix
    Object Pool + Bugfix

---
+++ v0.03.2/Assets/ObjectPool - Bullet/BulletTriggerHandler.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTriggerHandler : MonoBehaviour
+{
+    MissileAuto missileAuto;
+    BulletData bulletData;
+    PoolPosition poolPosition;
+
+    Transform target;
+    public Transform closestTarget = null;
+
+    private void Awake()
+    {
+        poolPosition = FindObjectOfType<PoolPosition>();
+        missileAuto = FindObjectOfType<MissileAuto>();
+        bulletData = GetComponent<BulletData>();
+
+    }
+    private void OnEnable()
+    {
+        transform.position = transform.parent.position;
+        FindClosestTarget();
+        transform.SetParent(null);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<IDamageable>().TakeDamage(bulletData.damage);
+            gameObject.SetActive(false);
+            transform.SetParent(poolPosition.transform);
+
+        }
+    }
+
+
+
+
+
+
+
+
+    // CLOSEST TARGET FIND && MISSILE MOVEMENT
+    public void FindClosestTarget()
+    {
+        StartCoroutine(FindClosestTargetENUM());
+    }
+    IEnumerator FindClosestTargetENUM()
+    {
+        while (true)
+        {
+
+            EnemyCollisionHandler[] enemies = FindObjectsOfType<EnemyCollisionHandler>();
+            float maxDistance = Mathf.Infinity;
+
+            foreach (EnemyCollisionHandler enemy in enemies)
+            {
+
+                float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);//vector3distance ın ilk degiskenini degistirdim.
+                if (targetDistance < maxDistance)
+                {
+                    closestTarget = enemy.transform;
+                    maxDistance = targetDistance;
+                }
+            }
+            target = closestTarget;
+            Debug.Log("missile gönderiliyor");
+
+            SendMissile();
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+
+    void SendMissile() 
+    {
+        StartCoroutine(MissileMovement(this.gameObject));
+    }
+    IEnumerator MissileMovement(GameObject createdMissile)
+    {
+        while (true)
+        {
+
+            if (createdMissile != null && closestTarget != null)
+            {
+
+
+                bulletData = createdMissile.GetComponent<BulletData>();// bulletdata yerine normal speed yazýp optimize edilebilir.
+                createdMissile.transform.localPosition += (closestTarget.transform.position - createdMissile.transform.position).normalized * bulletData.speed * Time.deltaTime;
+                createdMissile.transform.LookAt(closestTarget); // lerp ile yapmayý dene.
+
+                Transform temptarget = closestTarget;
+
+                if (temptarget == null)
+                {
+                    createdMissile.SetActive(false);
+                }
+
+            }
+            else if (closestTarget == null)
+            {
+                createdMissile.SetActive(false);
+
+                //FindClosestTarget();
+            }
+
+            yield return null;
+        }
+
+    }
+}
---
+++ v0.03.2/Assets/ObjectPool - Bullet/MissileAuto.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileAuto : MonoBehaviour
+{
+    public GameObject[] objectPool;
+    private void Start()
+    {
+        StartCoroutine(CreateMissile());
+    }
+    IEnumerator CreateMissile()
+    {
+        while (true)
+        {
+            for (int i = 0; i < objectPool.Length; i++)
+            {
+                objectPool[i].SetActive(true);
+                GameObject tempBullet = objectPool[i];
+               // StartCoroutine(SendMissile(tempBullet));
+
+
+                if (i == objectPool.Length - 1)
+                {
+                    i = 0;
+                }
+                yield return new WaitForSeconds(1);
+
+            }
+        }
+    }
+
+
+
+
+
+    // CLOSEST TARGET FIND
+
+
+
+
+
+
+
+
+
+
+}
